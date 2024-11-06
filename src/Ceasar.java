1bfe6e0035a446879483df53302fe640dabb7955
Besenlohntsich <bessim.hasani@gmail.com>
2024-11-06 19:05:16 +0100

    Überführen des UML Dieagramms

---
+++ src/Ceasar.java
@@ -0,0 +1,75 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Ceasar {
+
+    public static void verschluesseln()
+    {
+
+
+    }
+
+    public static void entschluesseln()
+    {
+
+
+    }
+
+    public static char zahlenZuBuchstaben(int pWert)
+    {
+
+
+        return 0;
+    }
+
+    public static int buchstabenZuZahlen(char pWert)
+    {
+
+
+        return 0;
+    }
+
+    public static String getGt()
+    {
+
+
+        return "idk yet";
+    }
+
+   public static void setGt(String pGt)
+   {
+
+
+
+   }
+
+   public static String getKt()
+    {
+
+
+        return "idk yet";
+    }
+
+    public static void setKt(String pKt)
+    {
+
+
+    }
+
+    public static int getSchluessel()
+    {
+
+
+        return 0;
+    }
+
+    public static void setSchluessel(int pSchluessel)
+    {
+
+
+    }
+
+
+
+
+
+}
