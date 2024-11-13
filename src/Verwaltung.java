2cfd8219b37bd2700f7f4ab0a55094f9b09209a2
besenlohntsich <bessim.hasani@gmail.com>
2024-11-13 10:57:04 +0100

    Experimental branch

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Verwaltung {
+
+    public static void Scanner(String[] Args) {
+        Scanner sc = new Scanner(System.in);
+        //eingabe wird durch den scanner "wahrgenommen", dieser wird initialisiert
+
+        System.out.println("Gebe eine Nachricht zum verschlüsseln ein:");
+        String Nachricht = sc.nextLine();
+        //user input (zu Verschlüsselende Zeichenkette)
+        System.out.print("Gebe den Schlüssel ein:");
+        int Schuessel = sc.nextInt();
+        //Verschiebung durch CAESAR
+        String GT = verschluesseln(Nachricht, Schuessel);
+
+        System.out.println("verschlüsselte Nachricht: " + GT);
+
+        sc.close();
+    }
 }
