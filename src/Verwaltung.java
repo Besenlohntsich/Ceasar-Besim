e4c0cd081598673b889c66541e8c1be525c74f8c
besenlohntsich <bessim.hasani@gmail.com>
2024-11-13 11:09:01 +0100

    Experimental Kt

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -7,12 +7,12 @@ public class Verwaltung {
         //eingabe wird durch den scanner "wahrgenommen", dieser wird initialisiert
 
         System.out.println("Gebe eine Nachricht zum verschlüsseln ein:");
-        String Nachricht = sc.nextLine();
+        String Kt = sc.nextLine();
         //user input (zu Verschlüsselende Zeichenkette)
         System.out.print("Gebe den Schlüssel ein:");
         int Schuessel = sc.nextInt();
         //Verschiebung durch CAESAR
-        String GT = verschluesseln(Nachricht, Schuessel);
+        String GT = verschluesseln(Kt, Schuessel);
 
         System.out.println("verschlüsselte Nachricht: " + GT);
 
